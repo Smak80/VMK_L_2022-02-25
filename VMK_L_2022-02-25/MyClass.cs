2cab24c1a5a8fe4fc769bfd3f55c28258a50f2e3
Sergey Makletsov <smaklets@kpfu.ru>
2022-02-25 14:35:12 +0300

    Добавьте файлы проекта.

---
+++ VMK_L_2022-02-25/MyClass.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VMK_L_2022_02_25
+{
+    public struct MyStruct
+    {
+        public int a;
+        public double x;
+
+        public MyStruct(int v1, double v2) : this()
+        {
+            a = v1;
+            x = v2;
+        }
+
+        public override string ToString()
+        {
+            return $"a={a}; x={x}";
+        }
+    }
+    public class MyClass
+    {
+        private List<MyStruct> l = new ();
+
+        public List<MyStruct> MyList
+        {
+            get => l;
+            set => l = value;
+        }
+
+        public void Add(MyStruct s)
+        {
+            MyList.Add(s);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            if (MyList.Count > 0)
+            {
+                foreach (var myStruct in MyList)
+                {
+                    sb.Append($"{myStruct}\n");
+                }
+            }
+            else
+            {
+                sb.Append("Список пуст\n");
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ VMK_L_2022-02-25/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using VMK_L_2022_02_25;
+
+Console.WriteLine("Hello, World!");
+MyStruct s1 = new MyStruct(1, 1.5);
+MyStruct s2 = new MyStruct(2, -9.65);
+var class1 = new MyClass();
+Console.WriteLine(class1);
+class1.Add(s1);
+Console.WriteLine(class1);
+class1.Add(s2);
+Console.WriteLine(class1);
